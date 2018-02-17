76c42ffd15d60b60ed9e4e6edd8a177667b2bf0f
Eric Freynhagen <massive@shitwa.re>
2018-02-17 23:08:36 +0100

    v1

---
+++ cycle.py
@@ -0,0 +1,26 @@
+import ts3
+import sqlite3
+import time
+
+n=0
+conn = sqlite3.connect('ts.db')
+c = conn.cursor()
+c.execute(n = SELECT count() FROM users)
+conn.close()
+while n <= nmax
+c = conn.cursor()
+c.execute(get api,row n)
+-> sqlite select "apikey" id n 
+
+				r = requests.get('https://api.guildwars2.com/v2/account?access_token=' + event.parsed[0]["world"].strip())
+                if r.json().get("world"):
+                    if r.json()["world"] == 2201:
+					nop
+				else 
+					ts3conn.servergroupdelclient(sgid=127, cldbid=id[0]['uid'])
+					c = conn.cursor()
+					c.execute(del line n)
+					conn.commit()
+					conn.close()
+				
+			n+1
