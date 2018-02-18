5bc05870cac589b33cc349bb54fae8e290ad1c91
Yannick Linke <invisi@0x0f.net>
2018-02-18 01:22:49 +0100

    Implemented clean up which should work:tm:

--- cycle.py
+++ cycle.py
@@ -1,26 +1,30 @@
-import ts3
 import sqlite3
-import time
 
-n=0
-conn = sqlite3.connect('ts.db')
-c = conn.cursor()
-c.execute(n = SELECT count() FROM users)
-conn.close()
-while n <= nmax
-c = conn.cursor()
-c.execute(get api,row n)
--> sqlite select "apikey" id n 
+import requests
+import ts3
 
-				r = requests.get('https://api.guildwars2.com/v2/account?access_token=' + event.parsed[0]["world"].strip())
-                if r.json().get("world"):
-                    if r.json()["world"] == 2201:
-					nop
-				else 
-					ts3conn.servergroupdelclient(sgid=127, cldbid=id[0]['uid'])
-					c = conn.cursor()
-					c.execute(del line n)
-					conn.commit()
-					conn.close()
-				
-			n+1
+if __name__ == '__main__':
+    ts3conn = ts3.query.TS3Connection("youriphere")
+    ts3conn.login(
+        client_login_name="API-Bot",
+        client_login_password="clientpwhere"
+    )
+    ts3conn.use(sid=1)
+    ts3conn.clientupdate(client_nickname='API-Bot1')
+
+    conn = sqlite3.connect('ts.db')
+    c = conn.cursor()
+    c.execute('SELECT id, apikey, tsid FROM users ORDER BY timestamp ASC')
+
+    user_delete = []
+
+    for row in c:
+        r = requests.get('https://api.guildswars2.com/v2/account?access_token={}'.format(row[1]))
+        r_json = r.json()
+        if not (r_json['world'] and r_json['world'] == 2201):
+            cldbid = ts3conn.clientgetdbidfromuid(cluid=row[2])
+            ts3conn.servergroupdelclient(sgid=127, cldbid=cldbid)
+
+            user_delete.append(row[0])
+
+    c.execute('DELETE FROM users WHERE id IN ({})'.format(''.join(user_delete)))
