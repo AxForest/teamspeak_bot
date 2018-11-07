8c04649b96a994164181a823b4a82e0012e7966b
Yannick Linke <invisi@0x0f.net>
2018-11-07 19:31:58 +0100

    Let the bot log to a file

--- bot.py
+++ bot.py
@@ -1,24 +1,26 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-import logging
+import logging.handlers
 import re
+import sys
 import traceback
+from pathlib import Path
 
 import mysql.connector as msql
 import requests
 import ts3
 
-from config import *
 from common import fetch_account
+from config import *
 
 
 def send_message(recipient: str, msg: str):
     try:
         ts3c.exec_('sendtextmessage', targetmode=1, target=recipient, msg=msg)
     except ts3.query.TS3Error:
-        logging.error('Seems like the user I tried to message vanished into thin air')
-        logging.error(traceback.format_exc())
+        logger.error('Seems like the user I tried to message vanished into thin air')
+        logger.error(traceback.format_exc())
 
 
 def handle_event():
@@ -59,7 +61,7 @@ def handle_event():
                         for server_group in server_groups:
                             try:
                                 ts3c.exec_('servergroupdelclient', sgid=server_group, cldbid=cldbid)
-                                logging.info(
+                                logger.info(
                                     'Removed user dbid:{} ({}) from group {}'.format(cldbid, result[0], server_group))
                             except ts3.TS3Error:
                                 # User most likely doesn't have the group
@@ -72,14 +74,14 @@ def handle_event():
                             'WHERE `ignored` = FALSE AND (`apikey` = %s OR `name` = %s)',
                             (match.group(1), json.get('name')))
                 msqlc.commit()
-                logging.info('{} ({}) marked previous instances of {} as ignored'.format(event[0]['invokername'],
-                                                                                         event[0]['invokeruid'],
-                                                                                         match.group(1)))
+                logger.info('{} ({}) marked previous instances of {} as ignored'.format(event[0]['invokername'],
+                                                                                        event[0]['invokeruid'],
+                                                                                        match.group(1)))
                 send_message(event[0]['invokerid'],
                              'Done! Rechte von {} vorherigen Nutzern entzogen.'.format(len(results)))
             except msql.Error as err:
-                logging.error('Failed to mark api key {} as ignored'.format(match.group(1)))
-                logging.error(traceback.print_exc())
+                logger.error('Failed to mark api key {} as ignored'.format(match.group(1)))
+                logger.error(traceback.print_exc())
                 raise err
             finally:
                 if cur:
@@ -119,7 +121,7 @@ def handle_event():
                                 (event[0]['invokeruid'], message, json.get('name')))
                     result = cur.fetchone()
                     if result[0] > 0:
-                        logging.warning(
+                        logger.warning(
                             'User {} ({}) tried to use an already registered API key/account. ({})'.format(
                                 event[0]['invokername'], event[0]['invokeruid'], result[1]))
                         send_message(event[0]['invokerid'],
@@ -136,17 +138,17 @@ def handle_event():
                     # Assign configured role
                     cldbid = ts3c.exec_('clientgetdbidfromuid', cluid=event[0]['invokeruid'])[0]['cldbid']
                     ts3c.exec_('servergroupaddclient', sgid=server['group_id'], cldbid=cldbid)
-                    logging.info('Assigned world {} to {} ({})'.format(server['name'], event[0]['invokername'],
-                                                                       event[0]['invokeruid']))
+                    logger.info('Assigned world {} to {} ({})'.format(server['name'], event[0]['invokername'],
+                                                                      event[0]['invokeruid']))
                 except (ts3.TS3Error, msql.Error) as err:
                     if isinstance(err, ts3.query.TS3QueryError) and err.args[0].error['id'] == '2561':
-                        logging.info(
+                        logger.info(
                             'User {} ({}) registered a second time for whatever reason'.format(event[0]['invokername'],
                                                                                                event[0]['invokeruid']))
                         send_message(event[0]['invokerid'], 'Sie haben bereits die passende Servergruppe!')
                     else:
-                        logging.error('Failed to assign server group to user uid:{}'.format(event[0]['invokeruid']))
-                        logging.error(traceback.format_exc())
+                        logger.error('Failed to assign server group to user uid:{}'.format(event[0]['invokeruid']))
+                        logger.error(traceback.format_exc())
                         send_message(event[0]['invokerid'],
                                      'Fehler beim Speichern des API-Keys. Bitte kontaktieren Sie einen Admin.')
                 finally:
@@ -155,7 +157,7 @@ def handle_event():
                     if msqlc:
                         msqlc.close()
             else:  # Invalid world on API
-                logging.warning(
+                logger.warning(
                     'User {} ({}) is currently on world {} and tried to register.'.format(event[0]['invokername'],
                                                                                           event[0]['invokeruid'],
                                                                                           world))
@@ -170,8 +172,19 @@ def handle_event():
 
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    if not Path('logs').exists():
+        Path('logs').mkdir()
+
     logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    hldr = logging.handlers.TimedRotatingFileHandler('logs/bot.log', when='W0', encoding='utf-8', backupCount=16)
+    fmt = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s", "%Y-%m-%d %H:%M:%S")
+    hldr.setFormatter(fmt)
+    logger.addHandler(hldr)
+    stream = logging.StreamHandler(sys.stdout)
+    stream.setFormatter(fmt)
+    stream.setLevel(logging.DEBUG)
+    logger.addHandler(stream)
 
     with ts3.query.TS3ServerConnection(
             '{}://{}:{}@{}'.format(TS3_PROTOCOL, CLIENT_USER, CLIENT_PASS, QUERY_HOST)) as ts3c:
